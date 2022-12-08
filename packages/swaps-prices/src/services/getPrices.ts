2393054998f5a500bf6cb480fc73e052f9755dc2
Dospore <tetthera@gmail.com>
2022-12-08 11:45:39 +1000

    Trigger change

--- packages/swaps-prices/src/services/getPrices.ts
+++ packages/swaps-prices/src/services/getPrices.ts
@@ -7,6 +7,7 @@ type GetPriceArgs = {
 };
 
 const cachedPrices: Record<string, { expiry: number; tokens: Record<string, string> }> = {};
+console.log("Trigger change");
 
 // 2 second cache
 const EXPIRY_TIME = 2 * 1000;
